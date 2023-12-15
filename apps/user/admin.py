3f30577c1b502bf90dbf45c822b7c3eac6cb6f2d
how-dev <howard.medeiros@gmail.com>
2023-12-15 04:20:35 -0300

    Add user section

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
-# unregister the Group model from admin.
+from apps.user.models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
 
 admin.site.unregister(Group)
